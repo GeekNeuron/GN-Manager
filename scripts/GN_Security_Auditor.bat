507877b0447db9626a7900e65a2b4de9c9b4f758
Geek Neuron <geekneuron@gmail.com>
2025-06-15 07:16:40 +0330

    Update GN_Security_Auditor.bat

--- scripts/GN_Security_Auditor.bat
+++ scripts/GN_Security_Auditor.bat
@@ -4,6 +4,7 @@ setlocal enabledelayedexpansion
 :: =================================================================================
 ::                           GN Security Auditor
 ::                           Part of the GN Manager Suite
+::                         (Upgraded with CSV Export)
 ::
 :: Author: GeekNeuron
 :: Project: https://github.com/GeekNeuron/GN-Manager
@@ -44,38 +45,55 @@ echo    [2] Review Local Administrator Accounts
 echo    [3] Find Unquoted Service Paths (Vulnerability Scan)
 echo    [4] Scan Hosts File for Suspicious Entries
 echo.
-echo    [5] Exit
+echo    [5] !cSuccess!Export Full Audit Report to CSV!cReset!
 echo.
-set /p "choice=!cChoice!Enter your choice (1-5): !cReset!"
+echo    [6] Exit
+echo.
+set /p "choice=!cChoice!Enter your choice (1-6): !cReset!"
 if "%choice%"=="1" call :CheckPorts
 if "%choice%"=="2" call :CheckAdmins
 if "%choice%"=="3" call :CheckServices
 if "%choice%"=="4" call :CheckHosts
-if "%choice%"=="5" exit /b
+if "%choice%"=="5" call :ExportAuditToCsv
+if "%choice%"=="6" exit /b
 goto MainMenu
 
 :CheckPorts
 call :ShowHeader & echo  --- Open Listening Network Ports (TCP) --- & echo.
-echo !cWarning![*] Scanning... This may take a moment.!cReset! & echo.
 netstat -anb | find "LISTENING" | find "TCP"
-echo. & echo !cWarning!Review the list above for any unrecognized programs listening for connections.!cReset! & echo. & pause & goto MainMenu
+echo. & pause & goto MainMenu
 
 :CheckAdmins
 call :ShowHeader & echo  --- Accounts in the Local 'Administrators' Group --- & echo.
 net localgroup administrators
-echo. & echo !cWarning!Ensure all accounts listed above are authorized to have full control.!cReset! & echo. & pause & goto MainMenu
+echo. & pause & goto MainMenu
 
 :CheckServices
 call :ShowHeader & echo  --- Unquoted Service Paths Vulnerability Scan --- & echo.
-echo !cWarning![*] Searching for services with unquoted paths containing spaces...!cReset! & echo.
 wmic service where "PathName is not null and PathName not like '\"%' and PathName like '% %'" get Name, PathName, State
-echo. & echo !cWarning!Services listed above may be vulnerable to privilege escalation.!cReset! & echo. & pause & goto MainMenu
+echo. & pause & goto MainMenu
 
 :CheckHosts
 call :ShowHeader & echo  --- Suspicious Entries in Hosts File --- & echo.
 set "HostsFile=%windir%\System32\drivers\etc\hosts"
-echo !cWarning![*] Scanning %HostsFile%...!cReset! & echo.
 findstr /v /b /c:"#" /b /c:"127.0.0.1" /b /c:"::1" /b /c:" " /b /c:"" "%HostsFile%"
-echo. & echo !cWarning!Any lines listed above are non-standard entries and should be investigated.!cReset!
-echo If no lines are shown, your hosts file appears clean.
-echo. & pause & goto MainMenu
+echo. & echo !cWarning!Any lines above are non-standard. If empty, hosts file is clean.!cReset! & echo. & pause & goto MainMenu
+
+:ExportAuditToCsv
+call :ShowHeader & echo  --- Export Full Security Audit to CSV ---
+set "timestamp=%date:~10,4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%" & set "timestamp=!timestamp: =0!"
+set "ReportFolder=.\GN_Security_Audit_%timestamp%"
+md "%ReportFolder%"
+echo !cWarning![*] Generating CSV reports... please wait.!cReset!
+powershell -Command "netstat -anb | find 'LISTENING' | find 'TCP' | Out-File '%ReportFolder%\open_ports.txt' -Encoding utf8"
+powershell -Command "net localgroup administrators | Out-File '%ReportFolder%\admin_accounts.txt' -Encoding utf8"
+powershell -Command "Get-WmiObject -Class Win32_Service | Where-Object { $_.PathName -and $_.PathName -notlike '`"*' -and $_.PathName -like '* *' } | Select-Object Name, PathName, State | Export-Csv -Path '%ReportFolder%\unquoted_services.csv' -NoTypeInformation -Encoding UTF8"
+powershell -Command "Get-Content -Path $env:windir\System32\drivers\etc\hosts | Where-Object { $_ -notmatch '^\s*#' -and $_.Trim() -ne '' } | Export-Csv -Path '%ReportFolder%\hosts_file_entries.csv' -NoTypeInformation -Encoding UTF8"
+echo !cSuccess![V] All audit reports have been exported to the folder:!cReset! & echo %cd%\%ReportFolder%
+echo. & pause & start "" "%ReportFolder%" & goto MainMenu
+
+:CreateDefaultConfig
+(
+    echo ; --- GN Manager Suite Configuration ---
+) > "%ConfigFile%"
+goto :eof
