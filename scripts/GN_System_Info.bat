599aa18cce62e437e436874273596bafc471a5b6
Geek Neuron <geekneuron@gmail.com>
2025-06-16 12:27:56 +0330

    Create GN_System_Info.bat

--- scripts/GN_System_Info.bat
+++ scripts/GN_System_Info.bat
@@ -1,13 +1,19 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: Enable ANSI codes for older Windows
+reg query "HKCU\Console" /v VirtualTerminalLevel >nul 2>&1
+if %errorlevel% neq 0 (
+    reg add "HKCU\Console" /v VirtualTerminalLevel /t REG_DWORD /d 0x1 /f >nul
+)
+
 :: =================================================================================
 ::                               GN System Info
 ::                           Part of the GN Manager Suite
 ::                         (Upgraded with CSV Export)
 ::
 :: Author: GeekNeuron
-:: Project: https://github.com/GeekNeuron/GN-Manager
+:: Project: https://github.com/GeekNeuron/GN-Manager   
 :: =================================================================================
 
 :: --- Common Management Core ---
@@ -35,96 +41,93 @@ goto MainMenu
 
 :ShowHeader
 cls & echo.
-echo !cTitle! ██████╗ ███╗   ██╗     ███████╗██╗   ██╗███╗   ██╗███████╗ ██████╗ !cReset!
-echo !cTitle!██╔════╝ ████╗  ██║     ██╔════╝╚██╗ ██╔╝████╗  ██║██╔════╝██╔═══██╗!cReset!
-echo !cTitle!██║  ███╗██╔██╗ ██║     ███████╗ ╚████╔╝ ██╔██╗ ██║█████╗  ██║   ██║!cReset!
-echo !cTitle!██║   ██║██║╚██╗██║     ╚════██║  ╚██╔╝  ██║╚██╗██║██╔══╝  ██║   ██║!cReset!
-echo !cTitle!╚██████╔╝██║ ╚████║     ███████║   ██║   ██║ ╚████║███████╗╚██████╔╝!cReset!
-echo !cTitle! ╚═════╝ ╚═╝  ╚═══╝     ╚══════╝   ╚═╝   ╚═╝  ╚═══╝╚══════╝ ╚═════╝ !cReset!
+echo !cTitle!       ██████╗ ███╗   ██╗     ███████╗██╗   ██╗███╗   ██╗███████╗ ██████╗ !cReset!
+echo !cTitle!      ██╔════╝ ████╗  ██║     ██╔════╝╚██╗ ██╔╝████╗  ██║██╔════╝██╔═══██╗!cReset!
+echo !cTitle!      ██║  ███╗██╔██╗ ██║     ███████╗ ╚████╔╝ ██╔██╗ ██║█████╗  ██║   ██║!cReset!
+echo !cTitle!      ██║   ██║██║╚██╗██║     ╚════██║  ╚██╔╝  ██║╚██╗██║██╔══╝  ██║   ██║!cReset!
+echo !cTitle!      ╚██████╔╝██║ ╚████║     ███████║   ██║   ██║ ╚████║███████╗╚██████╔╝!cReset!
+echo !cTitle!       ╚═════╝ ╚═╝  ╚═══╝     ╚══════╝   ╚═╝   ╚═╝  ╚═══╝╚══════╝ ╚═════╝ !cReset!
 echo !cTitle!                                System Info                                   !cReset!
 echo !cTitle!===============================================================================!cReset!
-echo !cWarning! Author: GeekNeuron                                Project: %cSuccess%https://github.com/GeekNeuron/GN-Manager%cReset!
+echo !cWarning! Author: GeekNeuron           Project: %cSuccess%https://github.com/GeekNeuron/GN-Manager
 echo !cTitle!===============================================================================!cReset!
 echo. & goto :eof
 
 :MainMenu
 call :ShowHeader
-echo                   !cTitle!System Information Reporter!cReset!
+echo [1] OS and System Summary
+echo [2] CPU Information
+echo [3] RAM Information
+echo [4] Disk Information
+echo [5] GPU Information
+echo [6] Network Information
+echo [7] Show All Information
+echo [8] Export Report to CSV
+echo [9] Exit
 echo.
-echo    [1] OS and System Summary
-echo    [2] CPU Information
-echo    [3] Memory (RAM) Information
-echo    [4] Disk and Storage Information
-echo    [5] Graphics Card (GPU) Information
-echo    [6] Network Adapters Information
-echo.
-echo    [7] !cWarning!Show ALL Information (Sequential)!cReset!
-echo    [8] !cSuccess!Export Full Report to CSV Files!cReset!
-echo.
-echo    [9] Exit
-echo.
+
 set /p "choice=!cChoice!Enter your choice (1-9): !cReset!"
-if "%choice%"=="1" call :GetOSInfo
-if "%choice%"=="2" call :GetCPUInfo
-if "%choice%"=="3" call :GetRAMInfo
-if "%choice%"=="4" call :GetDiskInfo
-if "%choice%"=="5" call :GetGPUInfo
-if "%choice%"=="6" call :GetNetworkInfo
-if "%choice%"=="7" call :ShowAll
-if "%choice%"=="8" call :ExportAllToCsv
+if "%choice%"=="1" goto GetOSInfo
+if "%choice%"=="2" goto GetCPUInfo
+if "%choice%"=="3" goto GetRAMInfo
+if "%choice%"=="4" goto GetDiskInfo
+if "%choice%"=="5" goto GetGPUInfo
+if "%choice%"=="6" goto GetNetworkInfo
+if "%choice%"=="7" goto ShowAll
+if "%choice%"=="8" goto ExportAllToCsv
 if "%choice%"=="9" exit /b
 goto MainMenu
 
 :GetOSInfo
-call :ShowHeader & echo  --- OS and System Summary --- & echo.
-echo !cSuccess!Operating System:!cReset!
+cls
+echo --- OS and System Summary ---
 wmic os get Caption, Version, BuildNumber, OSArchitecture, InstallDate, SerialNumber | findstr /r /v "^$"
 echo.
-echo !cSuccess!Computer System:!cReset!
-wmic computersystem get Manufacturer, Model, Name, TotalPhysicalMemory /value
-echo.
-pause & goto MainMenu
+pause
+goto MainMenu
 
 :GetCPUInfo
-call :ShowHeader & echo  --- CPU Information --- & echo.
+cls
+echo --- CPU Information ---
 wmic cpu get Name, NumberOfCores, NumberOfLogicalProcessors, MaxClockSpeed, L3CacheSize, Manufacturer /value
 echo.
-pause & goto MainMenu
+pause
+goto MainMenu
 
 :GetRAMInfo
-call :ShowHeader & echo  --- Memory (RAM) Information --- & echo.
-echo !cWarning!Listing individual RAM modules:!cReset!
+cls
+echo --- Memory (RAM) Information ---
 wmic memorychip get BankLabel, Capacity, Speed, Manufacturer, PartNumber | findstr /r /v "^$"
 echo.
-echo !cSuccess!Total Physical Memory:!cReset!
 wmic computersystem get TotalPhysicalMemory /value
 echo.
-pause & goto MainMenu
+pause
+goto MainMenu
 
 :GetDiskInfo
-call :ShowHeader & echo  --- Disk and Storage Information --- & echo.
-echo !cSuccess!Logical Disks (Partitions):!cReset!
+cls
+echo --- Disk Information ---
 wmic logicaldisk get DeviceID, VolumeName, FileSystem, Size, FreeSpace | findstr /r /v "^$"
-echo !cWarning!Note: Size and FreeSpace are in Bytes.!cReset!
-echo.
-echo !cSuccess!Physical Disk Drives:!cReset!
 wmic diskdrive get Model, Size, InterfaceType | findstr /r /v "^$"
 echo.
-pause & goto MainMenu
+pause
+goto MainMenu
 
 :GetGPUInfo
-call :ShowHeader & echo  --- Graphics Card (GPU) Information --- & echo.
+cls
+echo --- GPU Information ---
 wmic path win32_videocontroller get Name, DriverVersion, AdapterRAM, VideoProcessor | findstr /r /v "^$"
-echo !cWarning!Note: AdapterRAM is in Bytes.!cReset!
 echo.
-pause & goto MainMenu
+pause
+goto MainMenu
 
 :GetNetworkInfo
-call :ShowHeader & echo  --- Network Adapters Information --- & echo.
-echo !cWarning!Listing enabled IP adapters only:!cReset!
+cls
+echo --- Network Information ---
 wmic nicconfig where "IPEnabled=true" get Description, IPAddress, IPSubnet, DefaultIPGateway, MACAddress, DNSServerSearchOrder | findstr /r /v "^$"
 echo.
-pause & goto MainMenu
+pause
+goto MainMenu
 
 :ShowAll
 call :GetOSInfo
@@ -136,22 +139,35 @@ call :GetNetworkInfo
 goto MainMenu
 
 :ExportAllToCsv
-call :ShowHeader & echo  --- Export Full System Report to CSV ---
-set "timestamp=%date:~10,4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%" & set "timestamp=!timestamp: =0!"
-set "ReportFolder=.\GN_System_Report_%timestamp%"
+cls
+echo --- Exporting System Report ---
+set "timestamp=%date:~-4,4%%date:~-7,2%%date:~-10,2%_%time:~0,2%%time:~3,2%%time:~6,2%"
+set "timestamp=!timestamp: =0!"
+set "UserName=%USERNAME%"
+set "ReportFolder=C:\Users\!UserName!\GN\GN_System_Report_!timestamp!"
 md "%ReportFolder%"
+if not exist "%ReportFolder%" (
+    echo !cError![!] Failed to create report folder! & pause & goto MainMenu
+)
 echo !cWarning![*] Generating CSV reports... please wait.!cReset!
-powershell -Command "Get-WmiObject -Class Win32_OperatingSystem | Select-Object Caption, Version, BuildNumber, OSArchitecture, InstallDate, SerialNumber | Export-Csv -Path '%ReportFolder%\os_info.csv' -NoTypeInformation -Encoding UTF8"
-powershell -Command "Get-WmiObject -Class Win32_Processor | Select-Object Name, NumberOfCores, NumberOfLogicalProcessors, MaxClockSpeed | Export-Csv -Path '%ReportFolder%\cpu_info.csv' -NoTypeInformation -Encoding UTF8"
-powershell -Command "Get-WmiObject -Class Win32_PhysicalMemory | Select-Object BankLabel, @{Name='Capacity(GB)';E={[math]::Round($_.Capacity / 1GB, 2)}}, Speed, Manufacturer, PartNumber | Export-Csv -Path '%ReportFolder%\ram_info.csv' -NoTypeInformation -Encoding UTF8"
-powershell -Command "Get-WmiObject -Class Win32_LogicalDisk | Select-Object DeviceID, VolumeName, FileSystem, @{Name='Size(GB)';E={[math]::Round($_.Size / 1GB, 2)}}, @{Name='FreeSpace(GB)';E={[math]::Round($_.FreeSpace / 1GB, 2)}} | Export-Csv -Path '%ReportFolder%\disk_info.csv' -NoTypeInformation -Encoding UTF8"
-powershell -Command "Get-WmiObject -Class Win32_VideoController | Select-Object Name, DriverVersion, @{Name='AdapterRAM(MB)';E={[math]::Round($_.AdapterRAM / 1MB, 2)}} | Export-Csv -Path '%ReportFolder%\gpu_info.csv' -NoTypeInformation -Encoding UTF8"
-powershell -Command "Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter 'IPEnabled=True' | Select-Object Description, IPAddress, MACAddress | Export-Csv -Path '%ReportFolder%\network_info.csv' -NoTypeInformation -Encoding UTF8"
-echo !cSuccess![V] All system reports have been exported to the folder:!cReset! & echo %cd%\%ReportFolder%
-echo. & pause & start "" "%ReportFolder%" & goto MainMenu
+powershell -Command "try { Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object Caption, Version, BuildNumber, OSArchitecture, InstallDate, SerialNumber | Export-Csv -Path '%ReportFolder%\os_info.csv' -NoTypeInformation -Encoding UTF8 } catch { echo !cError![!] Error exporting os_info.csv: $_!cReset! }"
+powershell -Command "try { Get-CimInstance -ClassName Win32_Processor | Select-Object Name, NumberOfCores, NumberOfLogicalProcessors, MaxClockSpeed | Export-Csv -Path '%ReportFolder%\cpu_info.csv' -NoTypeInformation -Encoding UTF8 } catch { echo !cError![!] Error exporting cpu_info.csv: $_!cReset! }"
+powershell -Command "try { Get-CimInstance -ClassName Win32_PhysicalMemory | Select-Object BankLabel, @{Name='Capacity(GB)';E={[math]::Round($_.Capacity / (1024*1024*1024), 2)}}, Speed, Manufacturer, PartNumber | Export-Csv -Path '%ReportFolder%\ram_info.csv' -NoTypeInformation -Encoding UTF8 } catch { echo !cError![!] Error exporting ram_info.csv: $_!cReset! }"
+powershell -Command "try { Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object DeviceID, VolumeName, FileSystem, @{Name='Size(GB)';E={[math]::Round($_.Size / (1024*1024*1024), 2)}}, @{Name='FreeSpace(GB)';E={[math]::Round($_.FreeSpace / (1024*1024*1024), 2)}} | Export-Csv -Path '%ReportFolder%\disk_info.csv' -NoTypeInformation -Encoding UTF8 } catch { echo !cError![!] Error exporting disk_info.csv: $_!cReset! }"
+powershell -Command "try { Get-CimInstance -ClassName Win32_VideoController | Select-Object Name, DriverVersion, @{Name='AdapterRAM(MB)';E={[math]::Round($_.AdapterRAM / (1024*1024), 2)}} | Export-Csv -Path '%ReportFolder%\gpu_info.csv' -NoTypeInformation -Encoding UTF8 } catch { echo !cError![!] Error exporting gpu_info.csv: $_!cReset! }"
+powershell -Command "try { Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration -Filter 'IPEnabled=True' | Select-Object Description, IPAddress, MACAddress | Export-Csv -Path '%ReportFolder%\network_info.csv' -NoTypeInformation -Encoding UTF8 } catch { echo !cError![!] Error exporting network_info.csv: $_!cReset! }"
+echo !cSuccess!Export complete: %ReportFolder%!cReset!
+pause
+goto MainMenu
 
 :CreateDefaultConfig
 (
     echo ; --- GN Manager Suite Configuration ---
+    echo LogFile=GN_Manager_Log.txt
+    echo UsePowerShell=true
 ) > "%ConfigFile%"
 goto :eof
+
+:end
+endlocal
+exit /b
